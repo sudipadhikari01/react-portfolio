96e0e1d1bc32f2fab22804aaf27cf604e7cb2f9c
Sudip Adhikari <sudipadhikari@Sudips-Air.lan>
2024-02-04 14:02:32 -0600

    article 2 added

--- src/data/articles.js
+++ src/data/articles.js
@@ -8,12 +8,14 @@ function article_1() {
 
 	const codeSnippet = `
 	exports.handler = async (event) => {
-		// Your code logic here
 		return {
 		  statusCode: 200,
 		  body: JSON.stringify('Hello from Lambda!'),
 		};
 	  }; `;
+
+	  
+
 	return {
 		date: "03 Feb 2024",
 		title: "Building a Serverless Application",
@@ -144,21 +146,207 @@ function article_1() {
 }
 
 function article_2() {
+
+	const codeSnippet1 = `
+	  const currentTime = new Date();
+	  const currentHour = currentTime.getHours(); `;
+
+	  const codeSnippet2 = `
+	  let greetingMessage;
+
+	  if (currentHour < 12) {
+		greetingMessage = 'Good morning!';
+	  } else if (currentHour < 18) {
+		greetingMessage = 'Good afternoon!';
+	  } else {
+		greetingMessage = 'Good evening!';
+	  }
+	  `;
+
+	  const codeSnippet3 = `
+	  return (
+		<div>
+		  <h1>{greetingMessage}</h1>
+		  {/* Other content of my portfolio */}
+		</div>
+	  );
+	  `;
+
+	  const codeSnippet4 = `
+	  // Sample Lambda function to save contact form data in DynamoDB
+	  exports.handler = async (event) => {
+		  //code logic to save data in DynamoDB
+		  return {
+			statusCode: 200,
+			body: JSON.stringify('Form data saved successfully!'),
+		  };
+		};
+	  `;
+
+	  
+	
+  
+
+	  
+
+
 	return {
-		date: "7 May 2023",
-		title: "Artificial Intelligence in Healthcare",
+		date: "4 Feb 2024",
+		title: "A Dynamic Greeting on My Portfolio",
 		description:
-			"AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.",
+			"Bringing Personalization to the Digital Realm: A Dynamic Greeting on My Portfolio",
 		style: ``,
 		keywords: [
 			"Artificial Intelligence in Healthcare",
-			"Tharindu",
-			"Tharindu N",
-			"Tharindu Nayanajith",
+			"Sudip Adhikari",
+			"Sudip Adhikari",
+			"Sudip Adhikari",
 		],
 		body: (
 			<React.Fragment>
-				<h1>Content of article 2</h1>
+				
+				<div className="article-content">
+					
+					<div className="paragraph">
+						<div>
+							<br/>
+							<h3>Introduction</h3>
+								<p>
+									In the ever-evolving landscape of web development, the quest for creating unique and 
+									engaging user experiences remains paramount. Inspired by the idea of offering a
+									personalized touch to my portfolio, I embarked on a journey to implement a dynamic
+									greeting based on the time of day. In this article, I'll walk you through the thought
+									process, implementation steps, and the integration of a Lambda function for a 
+									seamless user interaction.
+								</p>
+						</div>
+
+						<div>
+							<h4>Inspiration: A Warm Welcome in Real-Time</h4>
+							<p>
+								The concept stemmed from the desire to go beyond the static and conventional 
+								elements of a portfolio. Imagine a website that welcomes visitors with a friendly
+								greeting tailored to the time of day — a digital handshake that adapts to the user's
+								experience. It's not just about displaying information; it's about creating an
+								atmosphere.
+							 </p>
+						</div>
+
+						<div>
+							<h3>Implementation Steps</h3>
+							<br/>
+								<div>
+									<h3>1. Identify the Time of Day</h3>
+										<p>
+											Using JavaScript, I tapped into the user's device to determine the current time. This 
+											crucial piece of information would serve as the foundation for crafting personalized
+											greetings.
+										</p>
+										<SyntaxHighlighter language="javascript" style={atomDark}>
+         							 		{codeSnippet1}
+       							 		</SyntaxHighlighter>
+								</div>
+
+								<div>
+									<br/>
+									<h3>2. Create Greeting Messages</h3>
+										<p>
+											With the time in hand, I designed different greeting messages for morning, afternoon, 
+											evening, and night. Each message would bring a unique flavor to the user's experience.
+										</p>
+										<SyntaxHighlighter language="javascript" style={atomDark}>
+         							 		{codeSnippet2}
+       							 		</SyntaxHighlighter>
+								</div>
+
+								<div>
+									<br/>
+									<h3>3. Display Dynamic Greeting</h3>
+										<p>
+											Incorporating React, I seamlessly integrated the dynamic greeting into the portfolio.  
+											The UI would now dynamically showcase a warm welcome based on the time of the 
+											user's visit.
+										</p>
+										<SyntaxHighlighter language="javascript" style={atomDark}>
+         							 		{codeSnippet3}
+       							 		</SyntaxHighlighter>
+								</div>
+
+								<div>
+									<br/>
+									<h3>4. Integrate Lambda Functionality</h3>
+										<p>
+											Building upon the existing Lambda function responsible for handling the contact form  
+											submissions, I ensured that the introduction of the dynamic greeting did not
+											compromise any existing functionalities.
+										</p>
+										<SyntaxHighlighter language="javascript" style={atomDark}>
+         							 		{codeSnippet4}
+       							 		</SyntaxHighlighter>
+								</div>
+						</div>
+
+						<div>
+						<br/>
+								<h4>Challenges and Solutions</h4>
+								<p>
+									No project is without its challenges. I encountered considerations related to time zones 
+									and ensuring that the dynamic greeting was not affected by discrepancies. Through
+									meticulous testing and adjustments, I addressed these challenges, creating a 
+									seamless and reliable experience for users across different regions.
+								</p>
+						</div>
+
+						<div>
+						<br/>
+								<h4>Testing and Deployment</h4>
+								<p>
+									After implementing the changes, thorough testing became imperative. I needed to 
+									ensure that the dynamic greeting functioned flawlessly and that the contact form 
+									functionalities were intact. Once confident in the stability, I deployed the updated 
+									version of my portfolio to the cloud.
+								</p>
+						</div>
+
+						<div>
+						<br/>
+								<h4>Conclusion</h4>
+								<p>
+									The implementation of a dynamic greeting on my portfolio not only adds a touch of
+									personalization but also exemplifies the evolving nature of web development. It's a 
+									testament to the power of combining creativity with technology, transforming a static
+									website into a dynamic and engaging digital space.
+								</p>
+								<p>
+									In conclusion, this project is a small but impactful step toward creating a more
+									immersive and welcoming digital environment. As technology continues to advance,  
+									the possibilities for personalization and user engagement are limitless.
+									
+								</p>
+						</div>
+
+						<div>
+						<br/>
+								<h4>Future Enhancements</h4>
+								<p>
+									Looking ahead, there's room for future enhancements. Considerations include
+									expanding the dynamic greetings based on user preferences, incorporating user- 
+									specific data for a more tailored experience, and exploring additional serverless 
+									functionalities to further elevate the portfolio's interactivity.
+								</p>
+								<p>
+									Bringing a dynamic greeting to my portfolio has not only added a personal touch but 
+									has also opened doors to future explorations in the realm of user-centric web 
+									development. As we embrace the dynamic nature of the digital world, the journey of 
+									crafting unique and engaging experiences continues.	
+								</p>
+						</div>
+
+
+
+
+					</div>
+				</div>
 			</React.Fragment>
 		),
 	};
